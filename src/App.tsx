60efc26ab5e9ed48eaa06426dc5762b2da2d318c
--list <12687466+CptSchnitz@users.noreply.github.com>
2020-07-28 14:29:19 +0300

    style(app): linted and fixed formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import './App.css';
 import ConflictsView from './conflicts/views/conflicts-view';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import {
-  ThemeProvider,
-  createMuiTheme,
-} from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 
-
-function App() {
+function App () {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
   const theme = React.useMemo(
@@ -19,14 +15,13 @@ function App() {
           type: prefersDarkMode ? 'dark' : 'light',
         },
       }),
-    [prefersDarkMode],
+    [prefersDarkMode]
   );
-
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <ConflictsView/>
+      <ConflictsView />
     </ThemeProvider>
   );
 }
